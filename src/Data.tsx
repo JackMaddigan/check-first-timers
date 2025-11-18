6dcb211a2195a753c50719126c5117e6bf17a34c
Jack Maddigan <jack.g.maddigan@gmail.com>
2025-11-19 01:52:04 +1300

    admin page

--- src/Data.tsx
+++ src/Data.tsx
@@ -2,20 +2,17 @@ import { useState } from 'react';
 import { useData } from './DataContext';
 
 export default function Data(){
-  const {importPersons, exportPersons, persons} = useData();
+  const {importPersons, exportPersons} = useData();
   const [value, setValue] = useState("");
 
   return (
-    <section>
-      <div>
-        <label htmlFor="import">Paste Import</label>
-        <textarea name='import' onChange={(e) => setValue(e.target.value)} value={value} />
-        <button onClick={() => importPersons(value)}>Import</button>
-        <button onClick={exportPersons}>Copy Export</button>
+    <section className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-2'>
+        <label className="text-sm font-bold text-gray-500" htmlFor="import">PASTE IMPORT</label>
+        <textarea name='import' onChange={(e) => setValue(e.target.value)} value={value} className='border border-gray-300 h-100' />
+        <button onClick={() => importPersons(value)} className='border border-green-800 text-green-800 bg-green-100 py-1 px-3 rounded-full self-start cursor-pointer'>Import</button>
       </div>
-      <div>
-        {JSON.stringify(persons, null, 2)}
-      </div>
+        <button onClick={exportPersons} className='border border-green-800 text-green-800 bg-green-100 py-1 px-3 rounded-full self-start cursor-pointer'>Copy Export</button>
     </section>
   );
 }
